8e5f1de77e5072ac53d7bccd6925bffe9e47b9cf
Adrian Szlegel <adrian@oaknode.com>
2020-03-16 15:10:59 +0100

    First component, pages and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// COMPONENTS
+import Navbar from './components/Navbar';
+
+// PAGES
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Social Media App</h1>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route exact path="/login" component={login} />
+            <Route exact path="/signup" component={signup} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
