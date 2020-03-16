784e832818c67745b4a8fe7046f8e61254b3ef5c
Adrian Szlegel <adrian@oaknode.com>
2020-03-16 22:52:16 +0100

    Material UI theme added and styled

--- src/App.js
+++ src/App.js
@@ -9,22 +9,45 @@ import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
+// MATERIAL UI
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
 import './App.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#546e7a',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+})
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={home} />
-            <Route exact path="/login" component={login} />
-            <Route exact path="/signup" component={signup} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
