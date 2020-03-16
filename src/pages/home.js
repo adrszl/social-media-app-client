f85973aacaabc503b28cdec152764c4337da6db9
Adrian Szlegel <adrian@oaknode.com>
2020-03-16 22:52:42 +0100

    Getting posts from the database

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
 
 class home extends Component {
+
+    state = {
+        screams: null
+    }
+
+    componentDidMount() {
+        axios.get('/screams')
+            .then((res) => {
+                this.setState({
+                    screams: res.data
+                })
+            })
+            .catch((err) => {
+                console.log('Error: ', err);
+            })
+    }
+
     render() {
+        let recentPostsMarkup = this.state.screams ? (
+            this.state.screams.map((post) => {
+                return <p>{post.body}</p>;
+            })
+        ) : <p>Loading...</p>
         return (
-            <div>
-                <h1>Home page</h1>
-            </div>
+            <Grid container spacing={16}>
+                <Grid item sm={8} xs={12}>
+                    {recentPostsMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile...</p>
+                </Grid>
+            </Grid>
         )
     }
 }
