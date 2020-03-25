801ad53aa4e0a37db7b7d4a712dcbad717b38ee8
Adrian Szlegel <adrian@oaknode.com>
2020-03-25 22:25:09 +0100

    Theme classes & jwt-decode installed

---
+++ src/util/theme.js
@@ -0,0 +1,42 @@
+export default 
+{
+    palette: {
+        primary: {
+        light: '#757ce8',
+        main: '#546e7a',
+        dark: '#002884',
+        contrastText: '#fff',
+        },
+        secondary: {
+        light: '#ff7961',
+        main: '#f44336',
+        dark: '#ba000d',
+        contrastText: '#000',
+        },
+    },
+    formPalette: {
+        form: {
+        textAlign: 'center'
+        },
+        image: {
+            margin: '20px auto'
+        },
+        pageTitle: {
+            fontSize: '3rem',
+            margin: '20px auto'
+        },
+        textField: {
+            margin: '10px auto'
+        },
+        button: {
+            position: 'relative'
+        },
+        progreee: {
+            position: 'absolute'
+        },
+        customError: {
+            color: 'red',
+            margin: '1rem auto'
+        }
+    }
+}
